0e12e73deb642a4b1cfaa2b4a861224ff68ef7ee
Nafis Handoko <naffisioner@gmail.com>
2022-03-23 17:31:05 +0700

    define global style for home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
+Color color1 = const Color.fromRGBO(238, 117, 73, 1);
+Color color2 = const Color.fromRGBO(255, 214, 200, 1);
+BorderRadius borderRadius1 = BorderRadius.circular(20);
+BoxShadow boxshadow1 = BoxShadow(
+    offset: const Offset(0, 4),
+    blurRadius: 20,
+    spreadRadius: 0,
+    color: color1.withOpacity(0.6));
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Aplikasi Dahar'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Text('Hello World')],
+        appBar: AppBar(
+          title: const Text('Aplikasi Dahar'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [Text('Hello World')],
+          ),
+        ),
+        bottomNavigationBar: const DaharNavBar());
   }
 }
 
