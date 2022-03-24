d0186d9819930694cff713658083303ee0b6794e
Nafis Handoko <naffisioner@gmail.com>
2022-03-24 19:06:33 +0700

    first commit on screen-item_detail

---
+++ lib/screens/item_detail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:dahar/global_styles.dart';
+
+class ItemDetail extends StatefulWidget {
+  const ItemDetail({Key? key}) : super(key: key);
+
+  @override
+  State<ItemDetail> createState() => _ItemDetailState();
+}
+
+class _ItemDetailState extends State<ItemDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
