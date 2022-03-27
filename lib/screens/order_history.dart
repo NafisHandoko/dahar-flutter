6f9378353b4902c77f0de6637f261e8328f3abf4
Nafis Handoko <naffisioner@gmail.com>
2022-03-28 06:45:04 +0700

    replace _historyItem reusable widget with HistoryItem statefulwidget

--- lib/screens/order_history.dart
+++ lib/screens/order_history.dart
@@ -12,7 +12,11 @@ class OrderHistory extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 25),
         color: Colors.white,
-        child: ListView(children: [_historyItem(), _historyItem()]),
+        child: ListView(children: const [
+          HistoryItem(
+            isConfirmed: false,
+          )
+        ]),
       ),
     );
   }
@@ -64,93 +68,128 @@ class HistoryAppBar extends StatelessWidget {
   }
 }
 
-Container _historyItem() {
-  return Container(
-    margin: const EdgeInsets.only(bottom: 20),
-    child: Row(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(right: 15),
-            width: 110,
-            height: 110,
-            decoration: BoxDecoration(
-                borderRadius: borderRadius1,
-                image: const DecorationImage(
-                    image: NetworkImage(
-                        'https://images.unsplash.com/photo-1572656631137-7935297eff55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'),
-                    fit: BoxFit.cover)),
-          ),
-          Expanded(
-            // color: Colors.amberAccent,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const Text(
-                  'Kari Spesial',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-                ),
-                Text(
-                  'Warung Bu Supiah',
-                  style: TextStyle(
-                      fontSize: 12, fontWeight: FontWeight.w400, color: color1),
-                ),
-                Text(
-                  'Rp 20.000 x 3',
-                  style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
-                      color: colorGrey),
-                ),
-                const Text(
-                  'Total Pesanan : Rp 60.000',
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: colorYellowStatusCont,
-                            borderRadius: borderRadius1),
-                        child: Text(
-                          'Belum dikonfirmasi',
-                          style: TextStyle(
-                              fontSize: 8,
-                              fontWeight: FontWeight.w500,
-                              color: colorYellowStatusText),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: borderRadius1,
-                            color: color1,
-                            boxShadow: [boxshadow1]),
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            padding: EdgeInsets.symmetric(horizontal: 10),
-                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            // maximumSize: Size(10, 10),
-                            // alignment: Alignment.centerLeft
-                          ),
-                          child: const Text(
-                            'Konfirmasi',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: () {},
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
+class HistoryItem extends StatefulWidget {
+  final String foodImage, foodName, foodSeller;
+  final int foodPrice, foodAmount;
+  final bool isConfirmed;
+  const HistoryItem(
+      {Key? key,
+      this.foodImage =
+          'https://images.unsplash.com/photo-1572656631137-7935297eff55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      this.foodName = 'Kari Spesial',
+      this.foodSeller = 'Warung Bu Supiah',
+      this.foodPrice = 20000,
+      this.foodAmount = 3,
+      required this.isConfirmed})
+      : super(key: key);
+
+  @override
+  State<HistoryItem> createState() => _HistoryItemState();
+}
+
+class _HistoryItemState extends State<HistoryItem> {
+  bool isConfirmed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isConfirmed = widget.isConfirmed;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 15),
+              width: 110,
+              height: 110,
+              decoration: BoxDecoration(
+                  borderRadius: borderRadius1,
+                  image: DecorationImage(
+                      image: NetworkImage(widget.foodImage),
+                      fit: BoxFit.cover)),
             ),
-          )
-        ]),
-  );
+            Expanded(
+              // color: Colors.amberAccent,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    widget.foodName,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    widget.foodSeller,
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: color1),
+                  ),
+                  Text(
+                    'Rp ${widget.foodPrice} x ${widget.foodAmount}',
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                        color: colorGrey),
+                  ),
+                  Text(
+                    'Total Pesanan : Rp ${widget.foodPrice * widget.foodAmount}',
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: colorYellowStatusCont,
+                              borderRadius: borderRadius1),
+                          child: Text(
+                            (isConfirmed ? 'Selesai' : 'Belum dikonfirmasi'),
+                            style: TextStyle(
+                                fontSize: 8,
+                                fontWeight: FontWeight.w500,
+                                color: colorYellowStatusText),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: borderRadius1,
+                              color: color1,
+                              boxShadow: [boxshadow1]),
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              // maximumSize: Size(10, 10),
+                              // alignment: Alignment.centerLeft
+                            ),
+                            child: Text(
+                              (isConfirmed ? 'Beri Rating' : 'Konfirmasi'),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                isConfirmed = true;
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]),
+    );
+  }
 }
