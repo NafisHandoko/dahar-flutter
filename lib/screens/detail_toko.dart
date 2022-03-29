99bc2a652d159ecea5784d33c3b7d852c4755cab
Nafis Handoko <naffisioner@gmail.com>
2022-03-29 13:36:22 +0700

    add almost the whole thing including _menuItem, Wrap widget, and floatingActionButton

--- lib/screens/detail_toko.dart
+++ lib/screens/detail_toko.dart
@@ -57,7 +57,7 @@ class DetailToko extends StatelessWidget {
                         image: NetworkImage(
                             "https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"),
                         fit: BoxFit.cover),
-                    borderRadius: BorderRadius.circular(100),
+                    shape: BoxShape.circle,
                     color: color1),
               ),
               Container(
--- lib/screens/menu_toko.dart
+++ lib/screens/menu_toko.dart
@@ -6,6 +6,73 @@ import 'package:dahar/components/navbar.dart';
 class MenuToko extends StatelessWidget {
   const MenuToko({Key? key}) : super(key: key);
 
+  Widget _menuItem(String foodImage, String foodName, String foodPrice) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 145,
+          height: 145,
+          decoration: BoxDecoration(
+              borderRadius: borderRadius1,
+              image: DecorationImage(
+                  image: NetworkImage(foodImage), fit: BoxFit.cover)),
+          child: Stack(children: [
+            Positioned(
+              bottom: 10,
+              left: 10,
+              child: Row(children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: colorBlueEdit,
+                        boxShadow: [boxshadow2]),
+                    child: Icon(
+                      Icons.edit,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: colorRedDelete,
+                        boxShadow: [boxshadow2]),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ),
+                )
+              ]),
+            ),
+          ]),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 5, bottom: 2),
+          child: Text(
+            foodName,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+        ),
+        Text(
+          foodPrice,
+          style: TextStyle(
+              fontSize: 12, fontWeight: FontWeight.w500, color: color1),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +81,129 @@ class MenuToko extends StatelessWidget {
           child: BackAppBar(
             title: 'Menu Toko',
           )),
-      body: Container(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        color: Colors.white,
+        child: ListView(children: [
+          Column(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(bottom: 10),
+                // margin: const EdgeInsets.only(right: 15),
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: NetworkImage(
+                            "https://images.unsplash.com/photo-1628258475456-0224b1e4225a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"),
+                        fit: BoxFit.cover),
+                    shape: BoxShape.circle,
+                    color: color1),
+              ),
+              Container(
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Dapur Rika',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: colorBlueEdit,
+                          boxShadow: [boxshadow2]),
+                      child: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                        size: 15,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Jalan Mawar Baru no 50 Banjarmasin',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: colorBlueEdit,
+                          boxShadow: [boxshadow2]),
+                      child: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                        size: 15,
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                    borderRadius: borderRadius2,
+                    color: color1,
+                    boxShadow: [boxshadow1]),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Daftar Pesanan',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Daganganmu',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+            ),
+          ),
+          Wrap(
+            spacing: 20,
+            runSpacing: 20,
+            children: [
+              _menuItem(
+                  'https://images.unsplash.com/photo-1572656631137-7935297eff55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                  'Kari Spesial',
+                  'Rp 20000'),
+              _menuItem(
+                  'https://images.unsplash.com/photo-1572656631137-7935297eff55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                  'Kari Spesial',
+                  'Rp 20000'),
+              _menuItem(
+                  'https://images.unsplash.com/photo-1572656631137-7935297eff55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                  'Kari Spesial',
+                  'Rp 20000'),
+              _menuItem(
+                  'https://images.unsplash.com/photo-1572656631137-7935297eff55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                  'Kari Spesial',
+                  'Rp 20000'),
+            ],
+          )
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: color1,
+        child: Icon(Icons.add),
+      ),
       bottomNavigationBar: const NavBar(),
     );
   }
