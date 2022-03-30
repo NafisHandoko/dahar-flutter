235a09ff753abec775b9cf996fffa2ade134ddd7
Priya Risky Qurniawan <80609729+Priyasan181@users.noreply.github.com>
2022-03-30 20:59:51 +0700

    ada kurang

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:dahar/global_styles.dart';
 
@@ -6,34 +8,57 @@ class register extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var name = TextEditingController();
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
+      body: ListView(
         children: [
+          Image.asset(
+            'assets/images/logo.png',
+            height: 200,
+            width: 200,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: ("Nama"),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: ("Email"),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: ("Password"),
+            ),
+          ),
           Container(
-            child: TextField(
-              controller: name,
-              decoration: InputDecoration(
-                hintText: "name",
-                prefixIcon: Icon(Icons.person, color: color1),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(
-                    width: 10,
-                    color: color1,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(
-                    width: 10,
-                    color: color1,
-                  ),
-                ),
+            width: Get.width,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text("Create Account"),
+              style: ElevatedButton.styleFrom(
+                primary: color1,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               ),
             ),
-          )
+          ),
+          Text(Or continue with),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.facebook),
+                color: color1,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.google),
+                color: color1,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          TextButton(onPressed: (){}, child: Text("already have an account?")),
         ],
       ),
     );
