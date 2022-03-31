9a3161d9f4840af7b9e550bc240ac5cd5bde6c72
Priya Risky Qurniawan <80609729+Priyasan181@users.noreply.github.com>
2022-03-31 10:17:20 +0700

    style judul

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -11,8 +11,12 @@ class register extends StatelessWidget {
         child: Container(
           child: Column(
             children: [
-              Text('Register'),
-              Text('sing up for free'),
+              Text(
+                'REGISTER',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              Text('sing up for free',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
               Container(),
               Container(),
               Container(),
