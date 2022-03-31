e27bf08525df38084b21c9894e49c1d6ac89e888
elaristi <elaristiela11@gmail.com>
2022-03-31 10:47:05 +0700

    tambah teks date, time, dan total expense

--- lib/screens/transaction_done.dart
+++ lib/screens/transaction_done.dart
@@ -11,25 +11,72 @@ class _TransactionDoneState extends State<TransactionDone> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Container(
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            Icon(
-              Icons.check_circle,
-              size: 100,
-              color: Colors.green,
-            ),
-            Text(
-              'Order Success!',
-              style: TextStyle(
-                fontSize: 50,
+      body: SafeArea(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Icon(
+                Icons.check_circle,
+                size: 100,
+                color: Colors.green,
               ),
-            )
-          ],
+              Text(
+                'Order Success!',
+                style: TextStyle(
+                  fontSize: 35,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Date',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    '23-02-2022',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Time',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    '12.35.45',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Total Expense',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'Rp. 78.000,-',
+                    style: TextStyle(fontSize: 18),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
