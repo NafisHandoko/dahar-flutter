c57cc166c83042df3895aa544987e9c8b120c6c2
Priya Risky Qurniawan <80609729+Priyasan181@users.noreply.github.com>
2022-03-31 10:42:56 +0700

    form

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,3 +1,4 @@
+import 'package:dahar/global_styles.dart';
 import 'package:flutter/material.dart';
 
 class register extends StatelessWidget {
@@ -9,6 +10,7 @@ class register extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
           child: Column(
             children: [
               Text(
@@ -17,9 +19,102 @@ class register extends StatelessWidget {
               ),
               Text('sing up for free',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
-              Container(),
-              Container(),
-              Container(),
+              Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(90, 108, 234, 0.07),
+                      blurRadius: 50,
+                      spreadRadius: 0,
+                      offset: Offset(12, 26),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: borderRadius1,
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'username123',
+                    labelText: 'Username',
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: color1,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(90, 108, 234, 0.07),
+                      blurRadius: 50,
+                      spreadRadius: 0,
+                      offset: Offset(12, 26),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: borderRadius1,
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'username123',
+                    labelText: 'Username',
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: color1,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(90, 108, 234, 0.07),
+                      blurRadius: 50,
+                      spreadRadius: 0,
+                      offset: Offset(12, 26),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: borderRadius1,
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'username123',
+                    labelText: 'Username',
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: color1,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
