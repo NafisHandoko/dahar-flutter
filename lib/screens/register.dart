ac3c00d886cf327608daf9e746262a1345cd51ba
Priya Risky Qurniawan <80609729+Priyasan181@users.noreply.github.com>
2022-03-31 10:13:53 +0700

    baru lagi

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:dahar/global_styles.dart';
 
 class register extends StatelessWidget {
   const register({Key? key}) : super(key: key);
@@ -7,57 +6,19 @@ class register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Image.asset(
-            'assets/images/logo.png',
-            height: 200,
-            width: 200,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              label: ("Nama"),
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              label: ("Email"),
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              label: ("Password"),
-            ),
-          ),
-          Container(
-            width: Get.width,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text("Create Account"),
-              style: ElevatedButton.styleFrom(
-                primary: color1,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              ),
-            ),
-          ),
-          Text(Or continue with),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          child: Column(
             children: [
-              IconButton(
-                icon: const Icon(Icons.facebook),
-                color: color1,
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.google),
-                color: color1,
-                onPressed: () {},
-              ),
+              Text('Register'),
+              Text('sing up for free'),
+              Container(),
+              Container(),
+              Container(),
             ],
           ),
-          TextButton(onPressed: (){}, child: Text("already have an account?")),
-        ],
+        ),
       ),
     );
   }
