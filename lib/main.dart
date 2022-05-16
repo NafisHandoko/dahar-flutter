815eb6477c31b20e3706c9f8bb0585f76c268e98
NafisHandoko <naffisioner@gmail.com>
2022-05-16 12:14:09 +0700

    initial route change to '/'

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        initialRoute: '/home',
+        initialRoute: '/',
         routes: {
           '/': (context) => const Wrapper(),
           '/home': (context) => const Home(),
