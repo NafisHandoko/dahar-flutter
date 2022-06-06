7cdf42d51f82ae40246e8f9396abeb13fd5b55a1
Nafis Handoko <naffisioner@gmail.com>
2022-06-06 08:46:45 +0700

    add cart screen

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:dahar/global_styles.dart';
+import 'package:dahar/components/navbar.dart';
+import 'package:dahar/components/back_appbar.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: BackAppBar(
+            title: 'Cart',
+          )),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        color: Colors.white,
+        child: Column(
+          children: [
+            ListView(
+              children: [CartItem()],
+            ),
+            Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CartItem extends StatefulWidget {
+  const CartItem({Key? key}) : super(key: key);
+
+  @override
+  State<CartItem> createState() => _CartItemState();
+}
+
+class _CartItemState extends State<CartItem> {
+  int _cartCount = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 15),
+              width: 110,
+              height: 110,
+              decoration: BoxDecoration(
+                  borderRadius: borderRadius1,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.unsplash.com/photo-1572656631137-7935297eff55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'),
+                      fit: BoxFit.cover)),
+            ),
+            Expanded(
+              // color: Colors.amberAccent,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    'Sate Asli Madura',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    'Rumah Sate Pak Yen',
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: color1),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Rp 20.000',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w700)),
+                        Container(
+                          // margin: const EdgeInsets.only(bottom: 20),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: color1, width: 2),
+                            borderRadius: borderRadius2,
+                          ),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: borderRadius2,
+                                      color: color1,
+                                      boxShadow: [boxshadow1]),
+                                  child: Row(
+                                    children: [
+                                      TextButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              _cartCount -= 1;
+                                            });
+                                          },
+                                          child: const Text(
+                                            '-',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          )),
+                                      Text(
+                                        '$_cartCount',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      TextButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              _cartCount += 1;
+                                            });
+                                          },
+                                          child: const Text(
+                                            '+',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ))
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: InkWell(
+                                    onTap: () {},
+                                    child: Icon(
+                                      Icons.delete,
+                                      color: Colors.white,
+                                      size: 15,
+                                    ),
+                                  ),
+                                )
+                              ]),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]),
+    );
+  }
+}
